80afe353334bcb11fda6244d4d55913746578314
Taster101 <Davontahenderson1@gmail.com>
2023-06-08 14:55:39 -0500

    finishing touches

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,34 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => { 
+  const editorDb = await openDB('jate',1);
+  const tx = editorDb.transaction('jate', 'readwrite');
+
+  const store = tx.objectStore('jate');
+
+  const request = store.put({id: 1,value: content})
+  const results = await request;
+  console.log('your item has been saved to the database' , results.value)
+
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const editorDb = await openDB('jate',1);
+  const tx = editorDb.transaction('jate', 'readonly');
+
+  const store = tx.objectStore('jate');
+
+  const request = store.get(1)
+  const results = await request;
+  if(results){
+    console.log('data recived' , results.value)
+
+  } else {
+    console.log('data not found')
+  }
+  return results?.value
+}
 
 initdb();
